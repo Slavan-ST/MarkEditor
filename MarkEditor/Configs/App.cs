b0681e3406db179aa88801e86f6beccba74c1f43
Slavan-ST <slavan990900000@gmail.com>
2025-09-11 09:51:46 +0700

    внёс шаблоны для конфигов и поправил пакеты

---
+++ MarkEditor/Configs/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkEditor.Configs
+{
+    internal class App
+    {
+        public string AppName { get; set; } = string.Empty;
+        public int TimeoutSeconds { get; set; }
+    }
+}
---
+++ MarkEditor/Configs/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkEditor.Configs
+{
+    internal class Database
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+    }
+}
